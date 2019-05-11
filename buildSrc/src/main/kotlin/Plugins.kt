36e0c2b154ee9f82e3ae9f6baff2a3584b97d165
YazanTarifi <yazantarifi98@gmail.com>
2019-05-11 17:02:58 +0300

    add spring boot deps

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -8,5 +8,19 @@ object Plugins {
 
     const val JavaLibraryPlugin = "java-library"
     const val AndroidLibraryPlugin = "com.android.library"
+    const val MavenPublishPlugin = "maven-publish"
+    const val JavaPlugin = "java"
+    const val AndroidAppPlugin = "com.android.application"
+    const val IdeaPlugin = "idea"
+    const val AndroidKotlinPlugin = "kotlin-android"
+    const val AndroidKotlinExtentions = "kotlin-android-extensions"
+    const val SpringBootPlugin = "org.springframework.boot"
+    const val SpringBootPluginmanagment = "io.spring.dependency-management"
+    const val AndroidMavenPlugin = "com.github.dcendents.android-maven"
+    const val Maven = "maven"
+    const val KotlinPlugin = "kotlin"
+    const val BintrayRelease = "com.novoda.bintray-release"
+    const val Bintray = "com.jfrog.bintray"
+    const val MavenPublish = "maven-publish"
 
 }
---
+++ buildSrc/src/main/kotlin/RootLibs.kt
@@ -0,0 +1,26 @@
+/**
+ *    Copyright [2019] [Yazan Tarifi]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+object RootLibs {
+
+    const val AndroidGradle = "com.android.tools.build:gradle:3.4.0"
+    const val Kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21"
+    const val AndroidMaven = "com.github.dcendents:android-maven-gradle-plugin:1.4.1"
+    const val Bintray: String = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4"
+    const val SpringBootPluginV2 : String = "org.springframework.boot:spring-boot-gradle-plugin:2.2.0.BUILD-SNAPSHOT"
+    const val BintrayGradle = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4"
+
+}
